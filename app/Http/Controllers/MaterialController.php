37fdecdd9b437a4c3a5db21fcbc9913469a018f1
Hisham <boussofhisham@gmail.com>
2026-06-04 00:46:46 +0200

    logica bestellen toegevoegd

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -12,4 +12,23 @@ class MaterialController extends Controller
             
         return view('pages.materiaal', compact('materials'));
     }
+
+    public function order(Request $request, $id) {
+    
+    $request->validate([
+        'quantity' => 'required|integer|min:1',
+        'material_id' => 'required',
+    ]);
+
+    $material = Material::findOrFail($id);
+
+    if ($request->quantity > $material->stock) {
+        return redirect()->back()->withErrors(['quantity' => 'Meer dan de volledige voorraad bestellen, is niet mogelijk!']);
+    }
+
+    $material->stock -= $request->quantity;
+    $material->save();
+
+    return redirect()->back()->with('Success', 'Materiaal in winkelmandje toegevoegd!');
+    }
 }
