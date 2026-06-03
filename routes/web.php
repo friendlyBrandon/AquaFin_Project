eabe8de3c48d35c47541371b458b1bd649b0ca77
Hisham <boussofhisham@gmail.com>
2026-06-04 00:46:30 +0200

    route toegeoegd voor bestellen test

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/materiaal', [MaterialController::class, 'index']);
+Route::post('/materiaal/{id}', [MaterialController::class, 'order']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
