7004006e48e6dd12df3f31bc1937db9ce22bce22
Hisham <boussofhisham@gmail.com>
2026-06-03 23:45:18 +0200

    naam, productnummer en stock aangevuld in material_migration

--- database/migrations/2026_06_02_124215_create_material_table.php
+++ database/migrations/2026_06_02_124215_create_material_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('material', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('name');
+            $table->string('product_number')->unique();
+            $table->integer('stock')->default(0);
         });
     }
 
