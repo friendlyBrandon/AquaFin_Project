f19964b3bc6ccc89a526fe9b103ecc98f5a927bc
samanthakarengera <karengerasamantha@gmail.com>
2026-06-04 10:55:12 +0200

    fixed navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,20 +6,16 @@
     </div>
 
     <div class="nav-center">
+        @auth
         <a href="#">Materials</a>
         <a href="#">Flood Risk Forecast</a>
         <a href="#">Messages</a>
-    </div>
-
-    <div class="nav-right">
-        @auth
-            <a href="#">🛒 Cart</a>
-            <a href="#">Profile</a>
-
-            <form method="POST" action="{{ route('logout') }}">
+        <a href="#">🛒 Cart</a>
+        <a href="{{ route('profile.edit') }}">Profile</a>
+        <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <button class="logout-btn">Logout</button>
-            </form>
+        </form>
        
         @endauth
     </div>
