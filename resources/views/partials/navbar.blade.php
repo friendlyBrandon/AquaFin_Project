6a52142837cd87d11bec3c725c2f52fec03806ed
Hisham <boussofhisham@gmail.com>
2026-06-04 11:00:53 +0200

    link materials navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,12 +6,8 @@
     </div>
 
     <div class="nav-center">
-<<<<<<< HEAD
-        <a href="/materials">Materials</a>
-=======
         @auth
-        <a href="#">Materials</a>
->>>>>>> f19964b3bc6ccc89a526fe9b103ecc98f5a927bc
+        <a href="/materials">Materials</a>
         <a href="#">Flood Risk Forecast</a>
         <a href="#">Messages</a>
         <a href="#">🛒 Cart</a>
