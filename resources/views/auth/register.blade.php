35181caf3dcfdb10586b50bd2d9d2881b84f52d0
Brandon <b.willemoons@gmail.com>
2026-06-05 14:38:41 +0200

    Dropdown menu instead of input text for provinces

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,24 +2,39 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Gemeente -->
+        <!-- Provincie -->
         <div>
             <x-input-label for="provincie" :value="__('Provincie')" />
-            <x-text-input id="provincie" class="block mt-1 w-full" type="text" name="provincie" :value="old('provincie')" required autofocus autocomplete="provincie" />
+
+            <select id="provincie" name="provincie" required
+                class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                <option value="">Selecteer een provincie</option>
+                <option value="Antwerpen" {{ old('provincie') == 'Antwerpen' ? 'selected' : '' }}>Antwerpen</option>
+                <option value="Oost-Vlaanderen" {{ old('provincie') == 'Oost-Vlaanderen' ? 'selected' : '' }}>
+                    Oost-Vlaanderen</option>
+                <option value="West-Vlaanderen" {{ old('provincie') == 'West-Vlaanderen' ? 'selected' : '' }}>
+                    West-Vlaanderen</option>
+                <option value="Vlaams-Brabant" {{ old('provincie') == 'Vlaams-Brabant' ? 'selected' : '' }}>Vlaams-Brabant
+                </option>
+                <option value="Limburg" {{ old('provincie') == 'Limburg' ? 'selected' : '' }}>Limburg</option>
+            </select>
+
             <x-input-error :messages="$errors->get('provincie')" class="mt-2" />
         </div>
 
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Full name -->
         <div class="mt-4">
             <x-input-label for="fullname" :value="__('Full name')" />
-            <x-text-input id="fullname" class="block mt-1 w-full" type="text" name="fullname" :value="old('fullname')" required autocomplete="fullname" />
+            <x-text-input id="fullname" class="block mt-1 w-full" type="text" name="fullname" :value="old('fullname')"
+                required autocomplete="fullname" />
             <x-input-error :messages="$errors->get('fullname')" class="mt-2" />
         </div>
 
@@ -27,10 +42,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -39,15 +52,15 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
