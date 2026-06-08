47506436b8fad5159162d6385d90c6c1f8851a39
samanthakarengera <karengerasamantha@gmail.com>
2026-06-08 15:03:04 +0200

    provincie in profile page

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -31,7 +31,43 @@
             </div>
         @endif
         <div>
-            <x-input-label for="username" :value="__('Username:')" />
+    <x-input-label for="provincie" :value="__('Provincie:')" />
+
+    <select id="provincie" name="provincie" required
+        class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+
+        <option value="">Selecteer een provincie</option>
+
+        <option value="Antwerpen"
+            {{ old('provincie', $user->provincie) == 'Antwerpen' ? 'selected' : '' }}>
+            Antwerpen
+        </option>
+
+        <option value="Oost-Vlaanderen"
+            {{ old('provincie', $user->provincie) == 'Oost-Vlaanderen' ? 'selected' : '' }}>
+            Oost-Vlaanderen
+        </option>
+
+        <option value="West-Vlaanderen"
+            {{ old('provincie', $user->provincie) == 'West-Vlaanderen' ? 'selected' : '' }}>
+            West-Vlaanderen
+        </option>
+
+        <option value="Vlaams-Brabant"
+            {{ old('provincie', $user->provincie) == 'Vlaams-Brabant' ? 'selected' : '' }}>
+            Vlaams-Brabant
+        </option>
+
+        <option value="Limburg"
+            {{ old('provincie', $user->provincie) == 'Limburg' ? 'selected' : '' }}>
+            Limburg
+        </option>
+    </select>
+
+    <x-input-error :messages="$errors->get('provincie')" class="mt-2" />
+</div>
+        <div>
+            <x-input-label for="username" :value="__('Gebruikersnaam:')" />
             <x-text-input id="username" type="text" class="mt-1 block w-full" :value="$user->username" readonly />
         </div>
     </form>
