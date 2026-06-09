ba0dad25f29b588c9ee6304f21d0d554169b6ed1
Ben-Fodda <ilias.benfodda@student.odisee.be>
2026-06-09 14:49:38 +0200

    Cart Model aangemaakt

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    
+    protected $fillable = [
+        'materiaal_id',
+        'aantal',
+    ];
+
+    protected $table = 'cart';
 }
